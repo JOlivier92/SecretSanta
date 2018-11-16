034ad27fc61be9a83e55eab930000e24ce0957df
Joseph Olivier <josepholivier92@gmail.com>
2018-11-16 15:47:36 -0800

    add redux-logger to package json

---
+++ routes/api/admins.js
@@ -0,0 +1,88 @@
+const Admin = require('../../models/Admin');
+const bcrypt = require('bcryptjs');
+const jsonwebtoken = require('jsonwebtoken');
+const passport = require('passport');
+const keys = require('../../config/keys');
+
+
+let errors = {};
+
+exports.current = function(req,res) {
+    res.json({
+        id: admin.id,
+        email: admin.email,
+        room_ids: admin.room_ids,
+    });
+}
+
+exports.register = function(req,res){
+
+    Admin.findOne({username: req.body.username}).then(admin => {
+        // if the admin already exists in the database, return 400 level error
+        if (admin) {
+            errors.name = 'admin already exists';
+            return res.status(400).json(errors)
+        } else {
+        // else, assign newAdmin to posted body data
+            const newAdmin = new Admin({
+                email: req.body.email,
+                room_ids: req.body.room_ids,
+            })
+            // generate salt with 10 iterations, pass errors / generated salt to CB
+            bcrypt.genSalt(10, (err, salt) => {
+                // attach salt to given password, pass errors / new hash to CB
+                bcrypt.hash(newAdmin.password, salt, (err, hash) => {
+                    if(err) throw err;
+                    // if no errors, save the admin to the database with generated hash
+                    newAdmin.password = hash;
+                    // if admin successfully saves, sign jsonwebtoken
+                    newAdmin.save()
+                    .then(admin => {
+                        const payload = {
+                            id: admin.id,
+                            email: admin.email,
+                            room_ids: admin.room_ids,
+                        };
+                        jsonwebtoken.sign(payload, keys.secretOrKey, { expiresIn: 3600 }, (err, token) => {
+                            res.json({
+                                success: true,
+                                token: 'Bearer ' + token
+                            })
+                        })
+                    })
+                })
+            })
+        }
+    });    
+}
+
+exports.login = function(req,res) {
+
+    const email = req.body.email;
+    const password = req.body.password;
+    Admin.findOne({email})
+    .then(admin => {
+        if(!admin) {
+            errors.email = "This admin doesn't exist";
+            return res.status(400).json(errors);
+        }
+        bcrypt.compare(password, admin.password).then(isMatch => {
+            if (isMatch) {
+                const payload = {
+                    id: admin.id,
+                    email: admin.email,
+                    room_ids: admin.room_ids
+                };
+                jsonwebtoken.sign(payload, keys.secretOrKey, {expiresIn: 3600}, (err, token) => {
+                    res.json({
+                        success: true,
+                        token: 'Bearer ' + token
+                    })
+                })
+            } else {
+                errors.password = "incorrect password"
+                return status(400).json(errors)
+            }
+        })
+    })    
+}

e87f2b1cd890b239cea63f3c3f07adb7126cb684
domobritton <domonicbritton@gmail.com>
2018-11-21 16:21:19 -0800

    Responsive issue fix

--- frontend/src/components/splash.jsx
+++ frontend/src/components/splash.jsx
@@ -3,22 +3,24 @@ import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
 import { logoutAdmin } from "../util/session_api_util";
+import styled from 'styled-components';
 
 const Splashpage = ({ loggedIn, logout }) => {
   if (loggedIn) {
     return (
-      <div className="splash">
-        <Link to="/" onClick={() => logout()}>
+      <Splash>
+        <StyledLink to="/" onClick={() => logout()}>
           Log Out
-        </Link>
-      </div>
+        </StyledLink>
+      </Splash>
     );
   } else {
     return (
-      <div className="splash">
-        <Link to="/signup">Sign Up</Link>
-        <Link to="/login">Log In</Link>
-      </div>
+      <Splash>
+        <StyledLink to="/signup">Sign Up</StyledLink>
+        <br />
+        <StyledLink to="/login">Log In</StyledLink>
+      </Splash>
     );
   }
 };
@@ -31,4 +33,20 @@ const mapDispatchToProps = dispatch => ({
 export default withRouter(
   connect(mapStateToProps, mapDispatchToProps)(Splashpage)
 );
+
+
+const Splash = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: #fff;
+  font-size: 22px;
+`;
+
+
 
